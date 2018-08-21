1ae252eb21b3e0feaf9ca1925e1b58d6a0a1c9c9
clairemacdonald <claire.macdonald@blis.com>
2018-08-21 18:54:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,11 +1,29 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+let index = 0;
 
 function init() {
   // Write your JavaScript code inside the init() function
 
- let claire = document.body.addEventListener("keydown", function(e){
-    (e.which);
-  })
+ document.body.addEventListener("keydown", function (e) {
+   (e.which); 
+  // const key =e.which;
+   const key = parseInt(e.detail || e.which);
+   
+   if (key === code[index]) {
+     index++;
+     
+   }
+   
+   if (index === code.length) {
+     alert("Hurray!");
+     
+     index = 0;
+   }
+    else {
+      index = 0;
+    }
+  
+  });
   
 
 
